6ed0373624cc51cbc793bc0f097d0ca5fee84c08
xmkabel <mkabel190@gmail.com>
2025-02-28 18:49:40 +0200

    Deploy for Live

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-physical-education',
-})
+  base: "/vite-react-deploy/", // YOUR REPO NAME HERE
+});
