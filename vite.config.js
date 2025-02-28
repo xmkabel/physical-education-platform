88f9cc881eae92cd4b553e9aa9e81fb330838a75
xmkabel <mkabel190@gmail.com>
2025-02-28 19:29:40 +0200

    Logo webp, Some Styling

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-deploy/", // YOUR REPO NAME HERE
+  base: "/react-physical-education", // YOUR REPO NAME HERE
 });
