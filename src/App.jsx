32df43c0e8af2057c8695646706d95431cb01e81
xmkabel <mkabel190@gmail.com>
2025-03-01 10:47:53 +0200

    Second Quiz Routing

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
 =======
       <Route path="/" element={<Home />} />
       <Route path="/quizes/first" element={<FirstQuiz />} />
+      <Route path="/quizes/second" element={<SecondExam />} />
     </Routes>
     </>
   )
