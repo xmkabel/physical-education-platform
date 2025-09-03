eef5fb093e2c2cc3b6bfc1ce858bbe870339d596
xmkabel <mkabel190@gmail.com>
2025-09-03 13:00:42 +0300

    fix: build error resolve (case sensitive dir)

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
-import axiosInstance from "../Services/axios";
+import axiosInstance from "../services/axios";
 
 export const AuthContext = createContext(null);
 
