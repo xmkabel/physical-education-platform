6701f381fa97f688c3192f16e6d01af6f8fcef0b
youssefhanyfawq3 <judjrhjg@gmail.com>
2025-09-08 02:42:54 +0300

    style: comment out unused quiz data and components

--- src/components/routes/FinalChapterRouters.jsx
+++ src/components/routes/FinalChapterRouters.jsx
@@ -1,9 +1,9 @@
 import { Route } from 'react-router-dom';
 import Quiz from '../quizes/Quiz';
 import quizData7_1 from '../../data/final/1.json';
-import quizData7_2 from '../../data/final/2.json';
-import quizData7_3 from '../../data/final/3.json';
-import FinalCards from '../ExamCards/FinalCards';
+// import quizData7_2 from '../../data/final/2.json';
+// import quizData7_3 from '../../data/final/3.json';
+// import FinalCards from '../ExamCards/FinalCards';
 
 const FinalRoutes = [
   // <Route key="finals" path="/exams/final/" element={<FinalCards />} />,
